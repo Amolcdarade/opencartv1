7648451cd9f06daec6e8ccd677e0f4170b72a3b5
Amolcdarade <127284098+Amolcdarade@users.noreply.github.com>
2023-03-08 12:31:19 +0530

    firest commit

---
+++ javaOpeneCart/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author AK
+ *
+ */
+module javaOpeneCart {
+}
---
+++ javaOpeneCart/src/testoakage/TestClass.java
@@ -0,0 +1,14 @@
+package testoakage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("testBase class");
+		System.out.println("pom.xml");
+		System.out.println("utility");
+
+
+	}
+
+}
